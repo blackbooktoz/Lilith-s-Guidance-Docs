d82f85a3681ad0d0468a98571ba80e76ef8ba698
Black <blackbook666@proton.me>
2026-01-06 11:59:07 -0800

    m

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import pagePlugin from "@pelagornis/page";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://blackbooktoz.github.io",
+	base: "/Lilith-s-Guidance-Docs/",
 	integrations: [
 		starlight({
 			title: "Lilith's Guidance",
@@ -15,7 +17,7 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'HTML5 Version', slug: 'versions/html5' },
-						{ label: 'iOS Version', slug: 'versions/ios' },
+						// { label: 'iOS Version', slug: 'versions/ios' },
 						{ label: 'Updating', slug: 'versions/updating' },
 						{ label: 'Creating Backups', slug: 'versions/backups' },
 					],
